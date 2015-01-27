8d20bb1281fe5771dca8fcb3591c55dc907ab63a
Louis Mau <lmau404@mtroyal.ca>
2015-01-27 15:56:10 -0700

    Started read file
    Added set functions for Lemming class
    Added reading in info for main class

--- src/lemming/Lemming.java
+++ src/lemming/Lemming.java
@@ -2,4 +2,23 @@ package lemming;
 
 public class Lemming {
 
+	public Lemming()
+	{
+		
+	}
+	
+	public void setX(int nextInt) {
+		
+	}
+
+	public void setY(int nextInt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setTime(int nextInt) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- src/lemming/LemmingRunner.java
+++ src/lemming/LemmingRunner.java
@@ -1,10 +1,44 @@
 package lemming;
 
+import java.io.File;
+import java.util.*;
+
 public class LemmingRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Reading in the file
+		readFile();
+		
 
 	}
+	public static void readFile()
+	{
+		File file = new File("test_level_01.txt");
+		Scanner reader = null;
+		try{
+			reader = new Scanner(file);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		int maxHeight = reader.nextInt();
+		int timeLimit = reader.nextInt();
+		int passLemming = reader.nextInt();
+		int amountOfLemmings = reader.nextInt();
+		
+		for (int c = 0; c < amountOfLemmings; c++)
+		{
+			Lemming temp = new Lemming();
+			temp.setX(reader.nextInt());
+			temp.setY(reader.nextInt());
+			temp.setTime(reader.nextInt());
+		}
+		
+		
+	}
+
 
 }
