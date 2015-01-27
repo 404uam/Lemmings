e547601f3033fd15110d99d870d9ae8b799654d7
Louis Mau <lmau404@mtroyal.ca>
2015-01-27 14:46:37 -0700

    Initial commit
    Initial creation of classes

---
+++ src/lemming/Lemming.java
@@ -0,0 +1,5 @@
+package lemming;
+
+public class Lemming {
+
+}
---
+++ src/lemming/LemmingRunner.java
@@ -0,0 +1,10 @@
+package lemming;
+
+public class LemmingRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
